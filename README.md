0d126ea722f9fd39d2d207e7896dd50e54bb037d
zq <1471690846@qq.com>
2020-07-15 23:40:31 +0800

    testproject

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
+<<<<<<< HEAD
 # myproject
+=======
+# myweb
+backend for WeChat Mini Program
+>>>>>>> 7c6a44c... Initial commit
---
+++ RemindMeApp/app/build/generated/aidl_source_output_dir/debug/out/com/example/endterm/GuardAidl.java
@@ -0,0 +1,183 @@
+/*
+ * This file is auto-generated.  DO NOT MODIFY.
+ */
+package com.example.endterm;
+// Declare any non-default types here with import statements
+
+public interface GuardAidl extends android.os.IInterface
+{
+  /** Default implementation for GuardAidl. */
+  public static class Default implements com.example.endterm.GuardAidl
+  {
+    /**
+         * Demonstrates some basic types that you can use as parameters
+         * and return values in AIDL.
+         */
+    @Override public void basicTypes(int anInt, long aLong, boolean aBoolean, float aFloat, double aDouble, java.lang.String aString) throws android.os.RemoteException
+    {
+    }
+    @Override public java.lang.String getName() throws android.os.RemoteException
+    {
+      return null;
+    }
+    @Override
+    public android.os.IBinder asBinder() {
+      return null;
+    }
+  }
+  /** Local-side IPC implementation stub class. */
+  public static abstract class Stub extends android.os.Binder implements com.example.endterm.GuardAidl
+  {
+    private static final java.lang.String DESCRIPTOR = "com.example.endterm.GuardAidl";
+    /** Construct the stub at attach it to the interface. */
+    public Stub()
+    {
+      this.attachInterface(this, DESCRIPTOR);
+    }
+    /**
+     * Cast an IBinder object into an com.example.endterm.GuardAidl interface,
+     * generating a proxy if needed.
+     */
+    public static com.example.endterm.GuardAidl asInterface(android.os.IBinder obj)
+    {
+      if ((obj==null)) {
+        return null;
+      }
+      android.os.IInterface iin = obj.queryLocalInterface(DESCRIPTOR);
+      if (((iin!=null)&&(iin instanceof com.example.endterm.GuardAidl))) {
+        return ((com.example.endterm.GuardAidl)iin);
+      }
+      return new com.example.endterm.GuardAidl.Stub.Proxy(obj);
+    }
+    @Override public android.os.IBinder asBinder()
+    {
+      return this;
+    }
+    @Override public boolean onTransact(int code, android.os.Parcel data, android.os.Parcel reply, int flags) throws android.os.RemoteException
+    {
+      java.lang.String descriptor = DESCRIPTOR;
+      switch (code)
+      {
+        case INTERFACE_TRANSACTION:
+        {
+          reply.writeString(descriptor);
+          return true;
+        }
+        case TRANSACTION_basicTypes:
+        {
+          data.enforceInterface(descriptor);
+          int _arg0;
+          _arg0 = data.readInt();
+          long _arg1;
+          _arg1 = data.readLong();
+          boolean _arg2;
+          _arg2 = (0!=data.readInt());
+          float _arg3;
+          _arg3 = data.readFloat();
+          double _arg4;
+          _arg4 = data.readDouble();
+          java.lang.String _arg5;
+          _arg5 = data.readString();
+          this.basicTypes(_arg0, _arg1, _arg2, _arg3, _arg4, _arg5);
+          reply.writeNoException();
+          return true;
+        }
+        case TRANSACTION_getName:
+        {
+          data.enforceInterface(descriptor);
+          java.lang.String _result = this.getName();
+          reply.writeNoException();
+          reply.writeString(_result);
+          return true;
+        }
+        default:
+        {
+          return super.onTransact(code, data, reply, flags);
+        }
+      }
+    }
+    private static class Proxy implements com.example.endterm.GuardAidl
+    {
+      private android.os.IBinder mRemote;
+      Proxy(android.os.IBinder remote)
+      {
+        mRemote = remote;
+      }
+      @Override public android.os.IBinder asBinder()
+      {
+        return mRemote;
+      }
+      public java.lang.String getInterfaceDescriptor()
+      {
+        return DESCRIPTOR;
+      }
+      /**
+           * Demonstrates some basic types that you can use as parameters
+           * and return values in AIDL.
+           */
+      @Override public void basicTypes(int anInt, long aLong, boolean aBoolean, float aFloat, double aDouble, java.lang.String aString) throws android.os.RemoteException
+      {
+        android.os.Parcel _data = android.os.Parcel.obtain();
+        android.os.Parcel _reply = android.os.Parcel.obtain();
+        try {
+          _data.writeInterfaceToken(DESCRIPTOR);
+          _data.writeInt(anInt);
+          _data.writeLong(aLong);
+          _data.writeInt(((aBoolean)?(1):(0)));
+          _data.writeFloat(aFloat);
+          _data.writeDouble(aDouble);
+          _data.writeString(aString);
+          boolean _status = mRemote.transact(Stub.TRANSACTION_basicTypes, _data, _reply, 0);
+          if (!_status && getDefaultImpl() != null) {
+            getDefaultImpl().basicTypes(anInt, aLong, aBoolean, aFloat, aDouble, aString);
+            return;
+          }
+          _reply.readException();
+        }
+        finally {
+          _reply.recycle();
+          _data.recycle();
+        }
+      }
+      @Override public java.lang.String getName() throws android.os.RemoteException
+      {
+        android.os.Parcel _data = android.os.Parcel.obtain();
+        android.os.Parcel _reply = android.os.Parcel.obtain();
+        java.lang.String _result;
+        try {
+          _data.writeInterfaceToken(DESCRIPTOR);
+          boolean _status = mRemote.transact(Stub.TRANSACTION_getName, _data, _reply, 0);
+          if (!_status && getDefaultImpl() != null) {
+            return getDefaultImpl().getName();
+          }
+          _reply.readException();
+          _result = _reply.readString();
+        }
+        finally {
+          _reply.recycle();
+          _data.recycle();
+        }
+        return _result;
+      }
+      public static com.example.endterm.GuardAidl sDefaultImpl;
+    }
+    static final int TRANSACTION_basicTypes = (android.os.IBinder.FIRST_CALL_TRANSACTION + 0);
+    static final int TRANSACTION_getName = (android.os.IBinder.FIRST_CALL_TRANSACTION + 1);
+    public static boolean setDefaultImpl(com.example.endterm.GuardAidl impl) {
+      if (Stub.Proxy.sDefaultImpl == null && impl != null) {
+        Stub.Proxy.sDefaultImpl = impl;
+        return true;
+      }
+      return false;
+    }
+    public static com.example.endterm.GuardAidl getDefaultImpl() {
+      return Stub.Proxy.sDefaultImpl;
+    }
+  }
+  /**
+       * Demonstrates some basic types that you can use as parameters
+       * and return values in AIDL.
+       */
+  public void basicTypes(int anInt, long aLong, boolean aBoolean, float aFloat, double aDouble, java.lang.String aString) throws android.os.RemoteException;
+  public java.lang.String getName() throws android.os.RemoteException;
+}
